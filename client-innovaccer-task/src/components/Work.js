bdde67107f677c1647f70491a1729216ca250a1e
Aakash Goel <goelaakash79@gmail.com>
2020-07-06 04:42:44 +0530

    :package: update: added modal for more information in the work section

--- client-innovaccer-task/src/components/Work.js
+++ client-innovaccer-task/src/components/Work.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from "react-modal";
 
 const Work = ({ data }) => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [dt, setDt] = useState({});
+
+	ReactModal.defaultStyles.content.width = 400;
+	ReactModal.defaultStyles.content.height = 400;
+
+	const customStyles = {
+		content: {
+			top: "50%",
+			left: "50%",
+			right: "auto",
+			bottom: "auto",
+			marginRight: "-50%",
+			transform: "translate(-50%, -50%)",
+			borderRadius: "8px",
+			border: "none",
+			boxShadow: "0 2px 4px rgba(0,0,0,0.2)"
+		}
+	};
+
 	return (
 		<div className="col-md-6">
 			<h3 className="section-heading">Work Experiences</h3>
@@ -8,7 +29,14 @@ const Work = ({ data }) => {
 				{data &&
 					data.map(work => {
 						return (
-							<li className="card" key={work.company}>
+							<li
+								className="card"
+								key={work.company}
+								onClick={() => {
+									setIsOpen(true);
+									setDt(work);
+								}}
+							>
 								<h5>{work.role}</h5>
 								<p>
 									<span className="tag">
@@ -20,6 +48,30 @@ const Work = ({ data }) => {
 						);
 					})}
 			</ul>
+			<ReactModal
+				style={customStyles}
+				isOpen={isOpen}
+				onRequestClose={() => setIsOpen(false)}
+				shouldCloseOnOverlayClick={true}
+				contentLabel="Minimal Modal Example"
+			>
+				<button
+					className="button"
+					onClick={() => setIsOpen(false)}
+					style={{ right: 0 }}
+				>
+					close
+				</button>
+				<hr />
+				<h4>{dt.role}</h4>
+				<p>
+					<span className="tag">
+						{dt.company} - {dt.location}
+					</span>
+				</p>
+				<p>{dt.more}</p>
+				<p className="description">Duration: {dt.time}</p>
+			</ReactModal>
 		</div>
 	);
 };
